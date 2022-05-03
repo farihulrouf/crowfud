487f76e88aaf4f0d8cb2cf94ddf107244481efd1
farihulrouf <farihulrouf@gmail.com>
2022-05-03 12:45:09 +0700

    structure

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 import (
-	"fmt"
 	"log"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"crowdgo/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func main() {	
+func main() {
+	/*	
     db, err := gorm.Open(sqlite.Open("main.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Db connection error")
@@ -15,17 +17,37 @@ func main() {
 	fmt.Println("Database Connection successed")
 
 	var users []user.User
-	length := len(users)
 
-	fmt.Println(length)
 
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
 
 	for _, user := range users {
 		fmt.Println(user.Name)
 		fmt.Println(user.Email)
 	}
+	*/
+	router := gin.Default()
+	router.GET("/handler", Handler)
+	router.Run(":8111")
+}
+
+func Handler(c *gin.Context) {
+    db, err := gorm.Open(sqlite.Open("main.db"), &gorm.Config{})
+    if err != nil {
+    	log.Fatal("Db connection error")
+    }
+
+    //fm.Println("Database Connection successed")
+
+    var users []user.User
+    db.Find(&users)
+    c.JSON(http.StatusOK, users)
+
+    // handler
+    // service
+    // repository
+    // db
+
+
 }
